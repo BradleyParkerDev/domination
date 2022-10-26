10ba445f374a034b729d0a2334014e9c48f83b25
BradleyParkerDev <bradleyscottparker@outlook.com>
2022-10-25 21:11:56 -0500

    hi

--- main.js
+++ main.js
@@ -1,33 +1,39 @@
 /////////////////////////////////////////////////
 // Part One - Querying DOM Elements
-
+/////////////////////////////////////////////////
 let unorderedList = document.querySelector('#arguments');
 lineThroughListItem(unorderedList);
-
 imageUpdate("image-1","https://image.shutterstock.com/image-photo/background-elephant-600w-133689230.jpg");
 imageUpdate("image-2","https://image.shutterstock.com/image-photo/great-white-shark-showing-teeth-600w-397799335.jpg");
 imageUpdate("image-3","https://image.shutterstock.com/image-photo/wild-african-cheetah-beautiful-mammal-600w-319654835.jpg");
-
 removeArgumentsListItem();
 removeArgumentsListItem();
-
 changeFontSize("x-large", "thing-1");
+/////////////////////////////////////////////////
+
+
 
 /////////////////////////////////////////////////
 // Part Two - DOM Elements as Function Parameters
+/////////////////////////////////////////////////
 let newImage = document.createElement('img');
 appendElement(newImage);
-
 let image3 = document.querySelector('#image-3')
 changeImageSize(image3);
-
 let image2 = document.querySelector('#image-2')
 invisibleClass(image2);
+/////////////////////////////////////////////////
+
+
 
 /////////////////////////////////////////////////
 // Part Three - Creating DOM Elements
+/////////////////////////////////////////////////
 let newLi = liFunct("New List Item");
 appendElement(newLi);
+
+
+/////////////////////////////////////////////////
 
 
 
@@ -37,8 +43,11 @@ My functions are below
 
 */
 
+
+
 /////////////////////////////////////////////////
-// Part One
+// Part One - Functions
+/////////////////////////////////////////////////
 // Line through function
 function lineThroughListItem(list){
     let item1 = document.querySelector('li');   
@@ -64,7 +73,12 @@ function changeFontSize(size, id){
     idString.style.fontSize = size;
 }
 /////////////////////////////////////////////////
-// Part Two
+
+
+
+/////////////////////////////////////////////////
+// Part Two - Functions
+/////////////////////////////////////////////////
 // Appends element to #arguments
 function appendElement(newElement){
     unorderedList.appendChild(newElement);
@@ -80,10 +94,18 @@ function invisibleClass(myElement){
     myElement.className = "invisible";
 }
 /////////////////////////////////////////////////
-// Part Three
+
+
+
+/////////////////////////////////////////////////
+// Part Three - Functions
+/////////////////////////////////////////////////
 // The function returns a <li>
 function liFunct(myStr){
     let listItem = document.createElement('li');
     listItem.innerText = myStr;
     return listItem;
 }
+
+
+/////////////////////////////////////////////////
